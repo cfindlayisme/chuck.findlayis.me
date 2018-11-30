21aefff649f91fc3aad6c8c35085d291c8613ec4
Chuck Findlay <chuck@findlayis.me>
2018-11-30 16:21:45 -0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function viewHome() {
+    $('#content').html('<ul class="lead list-group">\
+        <li class="list-group-item list-group-item-secondary">Broadcast Engineer in Fort McMurray, Alberta</li>\
+        <li class="list-group-item list-group-item-secondary">Hobbiest programmer & sysadmin</li>\
+    </ul>\
+    <br>\
+    <p class="lead">\
+        <a href="#" class="btn btn-lg btn-secondary" onclick="viewContact();">Contact</a>\
+    </p>');
+}
+
+function viewContact() {
+    $('#content').html('<p class="lead">E-Mail:</p>\
+        <ul class="lead list-group">\
+        <li class="list-group-item list-group-item-secondary">chuck@findlayis.me</li>\
+    </ul>');
+}
+
+function navSelected(selected) {
+    $('.nav-link').each(function() {
+        $(this).removeClass('active');
+    });
+    $(selected).addClass('active');
+}
