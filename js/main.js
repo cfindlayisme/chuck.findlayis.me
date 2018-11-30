c1cbaf444c96275370e9c02aebc997bfb871cfa6
Chuck Findlay <chuck@findlayis.me>
2018-11-30 16:44:01 -0700

    Adjusted list on pages a little to take up less width

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 function viewHome() {
     $('#content').html('<ul class="lead list-group">\
-        <li class="list-group-item list-group-item-secondary">Broadcast Engineer in Fort McMurray, Alberta</li>\
+        <li class="list-group-item list-group-item-secondary">Broadcast Engineer in Alberta</li>\
         <li class="list-group-item list-group-item-secondary">Hobbiest programmer & sysadmin</li>\
     </ul>\
+    <br>\
     <br>\
     <p class="lead">\
         <a href="#" class="btn btn-lg btn-secondary" onclick="viewContact();">Contact</a>\
