a9e0fff4a537431fa6865c33c6fbccc6d96b797a
Chuck Findlay <chuck@findlayis.me>
2018-12-01 03:07:30 -0700

    Email: on contact page and capitalize main page stuff

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 function viewHome() {
     $('#content').html('<ul class="lead list-group">\
         <li class="list-group-item list-group-item-secondary">Broadcast Engineer in Alberta</li>\
-        <li class="list-group-item list-group-item-secondary">Hobbiest programmer & sysadmin</li>\
+        <li class="list-group-item list-group-item-secondary">Hobbiest Programmer & Sysadmin</li>\
     </ul>');
 }
 
 function viewContact() {
     $('#content').html('<ul class="lead list-group">\
-        <li class="list-group-item list-group-item-secondary">chuck@findlayis.me</li>\
+        <li class="list-group-item list-group-item-secondary">Email: chuck@findlayis.me</li>\
     </ul>');
 }
 
